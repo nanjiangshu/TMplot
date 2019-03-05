19b2b712c1e1bec9d65d43d8ef265c8c7a433f01
nanjiangshu <nanjiang.shu@gmail.com>
2019-03-05 11:38:23 +0100

    clean myfunc.py

--- myfunc.py
+++ myfunc.py
@@ -2420,26 +2420,6 @@ def second_to_human(time_in_sec):#{{{
         return None
 #}}}
 
-
-def check_output(*popenargs, **kwargs):#{{{
-    r"""Run command with arguments and return its output as a byte string.
-    Backported from Python 2.7 as it's implemented as pure python on stdlib.
-    >>> check_output(['/usr/bin/python', '--version'])
-    Python 2.6.2
-    """
-    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
-    output, unused_err = process.communicate()
-    retcode = process.poll()
-    if retcode:
-        cmd = kwargs.get("args")
-        if cmd is None:
-            cmd = popenargs[0]
-        error = subprocess.CalledProcessError(retcode, cmd)
-        error.output = output
-        raise error
-    return output
-#}}}
-
 def IsURLExist(url, timeout=2):#{{{
     try:
         response = requests.get(url,timeout=timeout)
