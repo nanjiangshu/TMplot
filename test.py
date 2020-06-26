7f46f7da599ecb9ae40643136a8acbd79d540821
nanjiangshu <nanjiang.shu@gmail.com>
2020-06-26 11:56:20 +0200

    update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import myfunc
 rundir = os.path.dirname(os.path.realpath(__file__))
 # prints whether python is version 3 or not
 python_version = sys.version_info.major
@@ -12,9 +13,32 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+
+basedir = os.path.realpath("%s/../"%(rundir))
+
+progname=os.path.basename(sys.argv[0])
+general_usage = """ 
+usage: %s TESTMODE options
+"""%(sys.argv[0])
+
+numArgv = len(sys.argv)
+if numArgv <= 1:
+    print general_usage
+    sys.exit(1)
+TESTMODE=sys.argv[1]
+
 g_params = {}
-g_params['font_dir'] = "%s/fonts/truetype/ttf-dejavu/"%(rundir)
-g_params['font_size'] = 16
-fontpath = g_params['font_dir'] + "DejaVuSerif.ttf"
-print (fontpath)
-g_params['fntTMbox_label'] = ImageFont.truetype(fontpath, 10)
+
+if TESTMODE == "loadpil":
+    g_params['font_dir'] = "%s/fonts/truetype/ttf-dejavu/"%(rundir)
+    g_params['font_size'] = 16
+    fontpath = g_params['font_dir'] + "DejaVuSerif.ttf"
+    print (fontpath)
+    g_params['fntTMbox_label'] = ImageFont.truetype(fontpath, 10)
+
+if TESTMODE == "getgapposition":
+    topo = sys.argv[2]
+    posGAP = myfunc.GetGapPosition(topo)
+    print(posGAP)
+
+
